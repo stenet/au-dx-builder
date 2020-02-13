a666f2f136376e41cec09f1ee392269d53612e72
Stefan Heim <stefan.heim@hotmail.com>
2020-02-13 15:52:04 +0100

    added refresh with PowerShell

---
+++ refresh_metadata.ps1
@@ -0,0 +1,23 @@
+Remove-Item .\metadata.json
+
+Write-Host "loading new metadata.json"
+$url = "https://raw.githubusercontent.com/DevExpress/devextreme-angular/master/metadata/NGMetaData.json"
+$output = ".\metadata.json"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+Write-Host "updating packages.json"
+$version = npm show devextreme version
+$package = Get-Content .\package.json | ConvertFrom-Json
+
+$package.version = $version
+$package.peerDependencies.devextreme = $version
+$package.devDependencies.devextreme = $version
+
+$package | ConvertTo-Json | Set-Content .\package.json
+
+Write-Host "installing packages"
+npm i
+
+Write-Host "creating new version"
+npm run build-dx
+npm run start
